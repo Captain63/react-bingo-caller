a4e605d4e65af4dac661c23fcde09589485e4c7c
Captain63 <58921221+Captain63@users.noreply@github.com>
2022-01-05 23:12:14 -0500

    Consolidate function to one line

--- src/utils/generateRandomNum.js
+++ src/utils/generateRandomNum.js
@@ -1,5 +1,3 @@
-const generateRandomNum = length => {
-    return Math.floor(Math.random() * length);
-}
+const generateRandomNum = length => Math.floor(Math.random() * length);
 
 export default generateRandomNum;
