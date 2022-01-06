cf6882269c976ce025e152d93d43c66f48de255b
Captain63 <58921221+Captain63@users.noreply@github.com>
2022-01-05 23:08:36 -0500

    Build logic for generating bingo number with state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import generateRandomNum from "./utils/generateRandomNum";
+import generateBingoNums from "./utils/generateBingoNums";
 
 function App() {
+  const [bingoState, setBingoState] = useState({ currentNum: '', bingoNums: generateBingoNums() });
+
+  const updateCurrent = () => {
+    const index = generateRandomNum(bingoState.bingoNums.length);
+
+    setBingoState({
+      ...bingoState,
+      currentNum: bingoState.bingoNums.splice(index, 1)
+    })
+
+    console.log(bingoState.bingoNums.length);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1>Let's Call Some Bingo Numbers</h1>
+      <button onClick={updateCurrent}>Generate Bingo Number</button>
+      <h2>{bingoState.currentNum}</h2>
+    </header>
   );
 }
 
